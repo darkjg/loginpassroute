2643b7b95f640c2301481dae3e936f86b97ac357
Jorge <newdarkjg@gmail.com>
2024-01-18 12:22:41 +0100

    Correcion fuera de tiempo

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const app = express();
-
+const dotenv = require('dotenv');
+const middlewares = require('./middlewares');
 const routes = require('./routes');
-const PORT = 4000;
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const PORT = 4000;
+dotenv.config();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.PALABRA_SECRETA || 'secretoSuperSecreto',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+middlewares.setupAPP(app);
 
 routes.setup(app);
 
-
-
 app.use((req, res) => {
-    res.status(404).send('404 Página no encontrada')
-  })
+  res.status(404).send('404 Página no encontrada')
+})
+
 
 app.listen(PORT, () => {
     console.log(`Servidor en ejecución en http://localhost:${PORT}`);
